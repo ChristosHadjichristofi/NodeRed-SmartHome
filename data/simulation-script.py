657e1f6fa8814d2427ba3f570d7741822ba81fe4
Christos Hadjichristofi <40044042+ChristosHadjichristofi@users.noreply.github.com>
2021-12-30 23:28:33 +0200

    Complete v1 simulation script

---
+++ data/simulation-script.py
@@ -0,0 +1,32 @@
+import requests
+import json
+import time
+
+# kafka producer
+URL = "http://localhost:3000/events"
+# how many minutes the simulator waits in order to send again a new request
+MINS_INTERVAL = 3
+
+def main():
+    # read all simulation data
+    with open('events.json', 'r') as f:
+        events = f.read().strip().split('\n')
+
+    # for every event
+    for event in events[1:-1]:
+        # set it to the right format in order to be able to convert it to json
+        if event[-1] is ',':     e = '{' + event[:] + '}'
+        else:                    e = '{' + event    + '}'
+
+        # json.loads creates a dict if string is valid
+        data = json.loads(e)
+        # get the response and print it
+        r = requests.post(url = URL, data = data)
+        print("Response text is:", r.text)
+        
+        time.sleep(60 * MINS_INTERVAL)
+    
+    print("Simulation completed.")
+
+if __name__ == "__main__":
+    main()
