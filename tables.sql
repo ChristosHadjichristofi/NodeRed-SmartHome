6b1a8b3b6c858969398d51110f878ea735f53938
Christos Hadjichristofi <40044042+BeenCoding@users.noreply.github.com>
2022-01-13 21:43:19 +0200

    Add description column
    Add description column in every table of the database so as we can write what triggered the flow and eventually a record was added to the database (better logging)

--- tables.sql
+++ tables.sql
@@ -2,21 +2,24 @@ CREATE TABLE IF NOT EXISTS smoke_sensor(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	status boolean not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS light_sensor(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	lm int not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS motion_sensor(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	status boolean not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS temp_sensor(
@@ -24,28 +27,32 @@ CREATE TABLE IF NOT EXISTS temp_sensor(
 	name varchar(255) not null,
 	temp int not null,
 	temp_dif int not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS magnet_sensor(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	status boolean not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS alarm(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	status boolean not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS siren(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	status boolean not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS thermostat(
@@ -53,14 +60,16 @@ CREATE TABLE IF NOT EXISTS thermostat(
 	name varchar(255) not null,
 	status boolean not null,
 	temp int not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS lamp(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	status boolean not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 
@@ -68,7 +77,8 @@ CREATE TABLE IF NOT EXISTS vacuum(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	status boolean not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS air_condition(
@@ -76,20 +86,23 @@ CREATE TABLE IF NOT EXISTS air_condition(
 	name varchar(255) not null,
 	status boolean not null,
 	temp int not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS water_heater(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	status boolean not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
 CREATE TABLE IF NOT EXISTS switch(
 	id int not null auto_increment primary key,
 	name varchar(255) not null,
 	status boolean not null,
-	date_time varchar(100) 
+	date_time varchar(100),
+	description varchar(250)
 );
 
